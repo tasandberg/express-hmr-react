e9d426005f395203471d00c7c3cd71a09dcbfcb7
Tim Sandberg <tasandberg@gmail.com>
2017-12-03 21:44:37 -0800

    Move index to src.  Add counter component for state testing

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './app'
+
+function renderApp() {
+  render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+renderApp()
+
+if (module.hot) module.hot.accept('./app', () => renderApp())
