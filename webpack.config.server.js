50a9ef909ff37462fb631dbfc1204f131027724c
Tim Sandberg <tasandberg@gmail.com>
2017-12-04 14:01:08 -0800

    Add server webpack transpiling

---
+++ webpack.config.server.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack')
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const StartServerPlugin = require('start-server-webpack-plugin')
+
+module.exports = {
+  entry: ['./server/index'],
+  watch: true,
+  target: 'node',
+  externals: [ nodeExternals() ],
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new StartServerPlugin({
+      name: 'server.js'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": { BUILD_TARGET: JSON.stringify("server") }
+    })
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: 'server.js'
+  }
+}
