3eb2bef83c5d87110b6f12e6a991dbe830a39cb3
Tim Sandberg <tasandberg@gmail.com>
2017-12-05 09:43:34 -0800

    Reorganize, reorganize to disable HMR for server code. yech.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const app = express();
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server';
+import App from '../src/app.js'
+
+const app = express()
+const PORT = 3000
 
 if (process.env.NODE_ENV !== 'production') {
-  require('./dev-middleware')(app)
+  require('../dev-middleware')(app)
 } else {
   app.use('/assets', express.static('dist'))
 }
 
 app.get('/', function(req, res) {
-  res.send('<body><div id="root">Hello World</div><script src=\'assets/bundle.js\'></script></body>');
-});
+  res.send('<body><div id="root">Hello World?!</div><script src=\'assets/bundle.js\'></script></body>')
+})
 
-const server = app.listen(3000, function() {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(PORT, () => `Express Server listening on ${PORT}`)
